f6e61ec8b42cc9410d94c54eff546c28e392e88f
muyulpw <2750068317@qq.com>
2020-08-03 10:35:39 +0800

    ceshi

---
+++ app/src/main/java/com/test/zfbbill/textName.java
@@ -0,0 +1,21 @@
+package com.test.zfbbill;
+
+/**
+ * created by lpw
+ * on 2020/8/3
+ */
+public class textName {
+    private String name;
+
+    public textName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
